156201ca3fc2832cc6321efa96e5e2d1fc890cd9
Tarun Raghav <120802593+TarunRaghav369@users.noreply.github.com>
2024-11-26 07:28:17 +0000

    added contact page and add padding to the nav

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Contact from './Contact';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
       </Routes>
     </Router>
   );
